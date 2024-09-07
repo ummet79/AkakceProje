450087605fc1139fd68e0ef95ddd36e9ccd045ad
polat <adababa1672@gmail.com>
2024-09-07 22:45:46 +0300

    Hazırlık

---
+++ src/_19_UserStory/_01_pOZİTİF.java
@@ -0,0 +1,7 @@
+package _19_UserStory;
+
+public class _01_pOZİTİF {
+    public static void main(String[] args) {
+        System.out.println("FDSDSA");
+    }
+}
---
+++ src/_19_UserStory/sSDA.java
@@ -0,0 +1,7 @@
+package _19_UserStory;
+
+public class sSDA {
+    public static void main(String[] args) {
+        System.out.println("FGNASG");
+    }
+}
