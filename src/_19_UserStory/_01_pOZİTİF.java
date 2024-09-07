eb0cbbd68a72d1a548cd8dc9b8d89155159dbbb2
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2024-09-07 23:29:31 +0300

    User Story 1 hazır

--- src/_19_UserStory/_01_pOZİTİF.java
+++
@@ -1,7 +0,0 @@
-package _19_UserStory;
-
-public class _01_pOZİTİF {
-    public static void main(String[] args) {
-        System.out.println("FDSDSA");
-    }
-}
--- src/_19_UserStory/sSDA.java
+++
@@ -1,7 +0,0 @@
-package _19_UserStory;
-
-public class sSDA {
-    public static void main(String[] args) {
-        System.out.println("FGNASG");
-    }
-}
