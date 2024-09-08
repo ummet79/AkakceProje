2e6f2e00db34889faacf82f5858ec025f1b779e3
leyla.ozddmr <leyla_ozdemir@hotmail.com.tr>
2024-09-08 22:19:05 +0300

    User Story 4 ün 2. kısmı test edildi

---
+++ src/_04__UserStory/_02_Negative.java
@@ -0,0 +1,41 @@
+package _04__UserStory;
+
+import Utlity.BaseDriver;
+import Utlity.MyFunc;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class _02_Negative extends BaseDriver {
+
+    @Test
+    public void Test2(){
+
+        driver.get("https://www.akakce.com/");
+
+        WebElement loginButton=driver.findElement(By.xpath("//*[@href='/akakcem/giris/']"));
+        loginButton.click();
+        MyFunc.Bekle(1);
+
+        WebElement Email=driver.findElement(By.xpath("//input[@id='life']"));
+        Email.sendKeys("dinotox01@esterace.com");
+        MyFunc.Bekle(1);
+
+        WebElement password=driver.findElement(By.xpath("//input[@id='lifp']"));
+        password.sendKeys("1234");
+        MyFunc.Bekle(1);
+
+        WebElement GirişYap=driver.findElement(By.xpath("//input[@id='lfb']"));
+        GirişYap.click();
+        MyFunc.Bekle(1);
+
+        Assert.assertTrue("Bu Emaile ait kayıtlı hesap bulunamadı",Email.getText().contains("dinotox898@esterace.com"));
+
+        BekleKapat();
+
+
+    }
+
+
+}
